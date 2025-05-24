92d3b0a9d8d9f8fc9f98198205640978f2503970
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-05-25 03:00:22 +0800

    update the basic 07 files - files read

--- 01_BASIC/07_Files.c
+++ 01_BASIC/07_Files.c
@@ -16,6 +16,23 @@ int main() {
     {
         printf("That file was NOT deleted!");
     }
+
+    /* ------------------------------------------------------ */
+    /* ===================== Files Read ===================== */
+    /* ------------------------------------------------------ */
+    FILE *fp = NULL;
+    char buff[255];
+    
+    fp = fopen("text.txt", "r");
+    fscanf(fp, "%s", buff);
+    printf("1: %s\n", buff);
+
+    fgets(buff, 255, (FILE*)fp);
+    printf("2: %s\n", buff);
+
+    fgets(buff, 255, (FILE*)fp);
+    printf("3: %s\n", buff);
+    fclose(fp);
     
     return 0;
 }
