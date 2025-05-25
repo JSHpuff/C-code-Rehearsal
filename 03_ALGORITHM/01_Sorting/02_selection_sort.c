31bc34959a559b9f79391c8f19a505c15508727b
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-05-25 18:26:18 +0800

    update the selection sort

--- 03_ALGORITHM/01_Sorting/02_selection_sort.c
+++ 03_ALGORITHM/01_Sorting/02_selection_sort.c
@@ -6,6 +6,7 @@ void swap(int *a, int *b);
 int main(void) {
     int arr[] = {22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70};
     int len = sizeof(arr) / sizeof(arr[0]);
+    printf("len: %d\n", len);
 
     selection_sort(arr, len);
 
