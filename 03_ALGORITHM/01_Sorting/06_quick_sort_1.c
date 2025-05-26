b0dd6b94e62254d50f0c7b31b791e80e9dd3956b
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-05-26 23:18:14 +0800

    update the quick sort 1

--- 03_ALGORITHM/01_Sorting/06_quick_sort_1.c
+++ 03_ALGORITHM/01_Sorting/06_quick_sort_1.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/** Defines a struct Range representing a subarray to sort
+ *  with start and end as indices.
+ */
 typedef struct _Range {
     int start, end;
 } Range;
 
+/** Helper Function
+ *  return a new range with given start and end
+ */
 Range new_Range(int s, int e) {
     Range r;
     r.start = s;
@@ -18,14 +24,19 @@ void swap(int *x, int *y) {
 }
 
 void quick_sort(int arr[], const int len) {
+    /** Avoids proceeding if the array is empty or length is invalid */
     if (len <= 0) {
         return;
     }
 
+    /** Creates a stack r of Range objects
+     *  Initializes with the full array range [0, len-1]
+     */
     Range r[len];
     int p = 0;
     r[p++] = new_Range(0, len - 1);
 
+    /** Processes each range until the stack is empty */
     while (p > 0) {
         Range range = r[--p];
         if (range.start >= range.end) {
@@ -36,6 +47,7 @@ void quick_sort(int arr[], const int len) {
         int left = range.start, right = range.end;
 
         do {
+            /** Moves elements smaller than pivot to the left and larger to the right */
             while (arr[left] < mid) ++left;
             while (arr[right] > mid) --right;
 
