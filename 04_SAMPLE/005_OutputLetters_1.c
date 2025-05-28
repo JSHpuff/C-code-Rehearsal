80e2d64107d3223d23af6272fc30ee1bf55d805a
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-05-29 03:04:13 +0800

    update the samples 5 output letter 1

---
+++ 04_SAMPLE/005_OutputLetters_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    char letter = 'A';
+
+    for (int i = 0; i < 26; i++) {
+        printf("%c ", letter);
+        letter++
+    }
+
+    printf("\n");
+
+    return 0;
+}
