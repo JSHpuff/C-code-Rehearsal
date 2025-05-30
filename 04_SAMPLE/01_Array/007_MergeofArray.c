a26026ee162b85071cc4bbe99e3cb07db6f95226
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-05-30 17:37:38 +0800

    update sample 1 array 7 merge of array

---
+++ 04_SAMPLE/01_Array/007_MergeofArray.c
@@ -0,0 +1,29 @@
+#include <stdoi.h>
+
+int main(void) {
+    int array[10];
+    int even[5] = {0, 2, 4, 6, 8};
+    int odd[5] = {1, 3, 5, 7, 9};
+    int index = 0, e_len = 5, o_len = 5;
+
+    for (int loop = 0; loop < e_len; loop++) {
+        array[index] = even[loop];
+        index++;
+    }
+
+    for (loop = 0; loop < o_len; loop++) {
+        array[index] = odd[loop];
+        index++;
+    }
+
+    printf("\nEven -> ");
+    for (loop = 0; loop < e_len; loop++) printf(" %d", even[loop]);
+
+    printf("\nOdd -> ");
+    for (loop = 0; loop < o_len; loop++) printf(" %d", odd[loop]);
+
+    printf("\nMerge Array -> ");
+    for (loop = 0; loop < 10; loop++) printf(" %d", array[loop]);
+
+    return 0;
+}
