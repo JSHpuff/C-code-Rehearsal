009165f1ccd9da7c0545130947f2d8d5bed766ab
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-06-19 00:53:25 +0800

    optimize the codes in 2 library 1 stdlib usage 1

--- 02_LIBRARY_USING/001_stdlib_usage_1.c
+++ 02_LIBRARY_USING/001_stdlib_usage_1.c
@@ -2,22 +2,33 @@
 #include <stdlib.h>
 
 int main() {
-    const int SIZE = 10;
+    const int size = 10;
+
+    // Allocate memory for an integer array
     // int *array = (int*)malloc(10 * sizeof(int));
     /* Better Codes */
     /** Avoids hardcoding type int
      *  Safer during type changes.
      */
-    int *array = malloc(SIZE * sizeof(*array));
+    int *array = malloc(size * sizeof(*array));
 
-    if (array == NULL) {
-        perror("Memory allocation failed");
-        // return 1;
-        /* Better Codes */
+    // if (array == NULL) {
+    //     perror("Memory allocation failed");
+    //     // return 1;
+    //     /* Better Codes */
+    //     return EXIT_FAILURE;
+    // }
+    /* Better Codes */
+    /** fprintf:
+     *  Allows full control of error wording.
+     */
+    if (!array) {
+        fprintf(stderr, "Error: Memory allocation failed.\n");
         return EXIT_FAILURE;
     }
 
-    for (int i = 0; i < 10; i++) {
+    // Initialize and print the array
+    for (int i = 0; i < size; i++) {
         array[i] = i;
         printf("%d ", array[i]);
     }
