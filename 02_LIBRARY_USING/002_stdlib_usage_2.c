97ed3c16c7c5548a4c8d8b9c93e1c2ff4c63c628
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-06-25 03:13:17 +0800

    optimize 2 library usage 2 stdlib usage 2

--- 02_LIBRARY_USING/002_stdlib_usage_2.c
+++ 02_LIBRARY_USING/002_stdlib_usage_2.c
@@ -2,12 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    srand(time(NULL));
-    for (int i = 0; i < 10; i++) {
-        printf("%d ", rand() % 100);
+/** Better Codes */
+#define NUM_RANDOMS 10
+#define MAX_RANDOM_VALUE 100
+
+int main(void) {
+    // srand(time(NULL));
+    /** Better Codes */
+    srand((unsigned int)time(NULL));
+
+    // for (int i = 0; i < 10; i++) {
+    /** Better Codes */
+    for (int i = 0; i < NUM_RANDOMS; i++) {
+        // printf("%d ", rand() % 100);
+        /** Better Codes
+         *  rand(): 0 ~ 32767
+         *  rand() % 100 = 0 ~ 99
+         */
+        printf("%d ", rand() % MAX_RANDOM_VALUE);
     }
-    printf("\n");
+    // printf("\n");
+    /** Better Codes
+     *  putchar: 
+     *  - print a single character
+     *  - the speed is faster than printf
+     */
+    putchar("\n");
 
     return 0;
 }
