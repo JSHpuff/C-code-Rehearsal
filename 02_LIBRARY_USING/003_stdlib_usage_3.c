ed92959509b2d4d49030146047c386f4c0561196
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-07-08 01:11:20 +0800

    update

--- 02_LIBRARY_USING/003_stdlib_usage_3.c
+++ 02_LIBRARY_USING/003_stdlib_usage_3.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
 
 int main() {
     const char *str = "12345";
-    int num = atoi(str);
-    printf("The integer value is %d\n", num);
+    char *endptr;
+    errno = 0;
+
+    long val = strtol(str, &endptr, 10);
+    
+    if (errno == ERANGE || val > INT_MAX || val < INT_MIN) {
+        perror("Integer overflow/underflow");
+        return EXIT_FAILURE;
+    }
+
+    if (*endptr != '\0') {
+        fprintf(stderr, "Invalid characters found: '%s'\n", endptr);
+        return EXIT_FAILURE;
+    }
+
+    printf("The integer value is %d\n", (int)val);
 
     return 0;
 }
