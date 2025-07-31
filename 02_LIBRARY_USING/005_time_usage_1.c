042e93da5ceb09471bbb969ab365e73ea1c06200
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-07-31 22:49:27 +0800

    update

--- 02_LIBRARY_USING/005_time_usage_1.c
+++ 02_LIBRARY_USING/005_time_usage_1.c
@@ -2,9 +2,21 @@
 #include <time.h>
 
 int main(void) {
-    time_t current_time;
-    time(&current_time);
-    printf("Current time: %s", ctime(&current_time));
+    // 宣告 time_t 型別的變數，用來存儲「1970 年到現在的秒數」
+    // time_t current_time;
+
+    // 把現在的時間寫進 current_time
+    // time(&current_time);
+    
+    // Optimize:
+    time_t now = time(NULL);
+    if (now == ((time_t) - 1)) {
+        perror("time failed");
+        return 1;
+    }
+    
+    // ctime() 會把 time_t 格式轉成字串
+    printf("Current time: %s", ctime(&now));
 
     return 0;
 }
